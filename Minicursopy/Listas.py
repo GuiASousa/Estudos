ac3222e3b0c7f31e37b44ff3c916a8ce1ffe241c
Guilherme Augusto de Sousa <31078186+GuiASousa@users.noreply.github.com>
2017-10-05 10:09:37 -0300

    Update Listas.py

--- Minicursopy/Listas.py
+++ Minicursopy/Listas.py
@@ -1,3 +1,11 @@
-l = [2 ,3 ,5 , True, 'Casa']
-for x in range(0,10):
-    print (x)
+def crivo(n):
+    c = []
+    d = []
+    for x in range(2, n):
+        if d.count(x) == 0:
+            c.append(x)
+            for y in range(1,n // x+1):
+                if d.count(x * y) == 0: d.append(x * y)
+    return c
+
+print(crivo(10))
