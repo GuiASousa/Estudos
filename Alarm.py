0d054f930fe481c302490ade0e05cca5c44265c3
Guilherme Augusto de Sousa <31078186+GuiASousa@users.noreply.github.com>
2018-01-03 21:51:22 -0200

    Update Alarm.py

--- Alarm.py
+++ Alarm.py
@@ -3,13 +3,11 @@ h = int(input("Horas: "))
 m = int(input("Minutos: "))
 s = int(input("Segundos: "))
 f = 0
-while True:
+while f == 0:
     a = datetime.now()
     b = a.hour
     c = a.minute
     d = a.second
-    if f == 1 :
-        break
-    elif h == b and m == c and s == d:
-        print("Ai sim Boy")
+    if h == b and m == c and s == d:
+        print("Bip!!!")
         f = 1
